0cb20d05551f6d72d065a3b8a804f7afe15bb792
Nicolas <73864341+FISHARMNIC@users.noreply.github.com>
2022-12-10 07:30:26 -0800

    Delete CHTTPserver directory

--- CHTTPserver/dispose.sh
+++
@@ -1,2 +0,0 @@
-# Run this to kill the server
-sudo kill -9 $(sudo lsof -ti:8080)
--- CHTTPserver/example.sh
+++
@@ -1,8 +0,0 @@
-gcc example/example.c -o example/main -Wall -Wextra
-if [ $? ]; then
-cd example 
-./main
-echo "---exit code: $?---"
-cd ../
-./dispose.sh
-fi
